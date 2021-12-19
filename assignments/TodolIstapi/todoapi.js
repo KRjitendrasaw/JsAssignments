adf22545f8e08b76e71c984d9985551334146785
Jitu Raj <jitu@raj.com>
2021-12-19 20:37:37 +0530

    todo_api

--- assignments/TodolIstapi/todoapi.js
+++ assignments/TodolIstapi/todoapi.js
@@ -1,23 +1,59 @@
 
-const completed_tasks = [];
-const pending_tasks = [];
+const api_url = "https://jsonplaceholder.typicode.com/todos";
+var true_btn=document.getElementsByTagName("button")[0];
+var false_btn=document.getElementsByTagName('button')[1];
 
-async function todo_api(){
-    const response = await fetch('`https://jsonplaceholder.typicode.com/todos'); 
-    const user = await response.json();
-    console.log(user);
-    
-    for( let i=0; i<user.length; i++){
-        if(user[i]['completed']==='true'){
-            completed_tasks.push(user[i]);
-        }
-        else{
-            pending_tasks.push(user[i]);
-        }
+var class_name=document.getElementsByClassName("todos")[0];
 
+async function getapi(url) {
+    const response = await fetch(url);
+    let data=await response.json();
+    let true_arr=[];
+    let false_arr=[];
+    for(var i=0;i<data.length;i++){
+        if(data[i]["completed"]==true){
+            true_arr.push(data[i]);
+        }else{
+            false_arr.push(data[i]);
+        }
     }
-    const a = todo_api();
-    console.log(a)
-    a.then(data => console.log(data))
-    
+    true_btn.addEventListener("click",()=>{
+        let count=0;
+        var html="<table border='1|1'>";
+        html+="<tr><th>Sr.No.</th><th>UserId</th><th>id</th><th>title</th><th>completed</th></tr>";
+        for(var i=0;i<true_arr.length;i++){
+            count++;
+            html+="<tr>";
+
+            html+="<td>"+count+"</td>";
+            html+="<td>"+true_arr[i].userId+"</td>";
+            html+="<td>"+true_arr[i].id+"</td>";
+            html+="<td>"+true_arr[i].title+"</td>";
+            html+="<td style=background-color:#00FF00>"+true_arr[i].completed+"</td>";
+            html+="</tr>";
+        }
+        html+="</table>"
+        class_name.innerHTML=html;
+    });
+
+
+    false_btn.addEventListener("click",()=>{
+        let count2=0;
+        var html="<table border='1|1' >";
+        html+="<tr><th>Sr.No.</th><th>UserId</th><th>id</th><th>title</th><th>completed</th></tr>";
+        for(var i=0;i<false_arr.length;i++){
+            count2++;
+            html+="<tr>";
+            html+="<td>"+count2+"</td>";
+            html+="<td>"+false_arr[i].userId+"</td>";
+            html+="<td>"+false_arr[i].id+"</td>";
+            html+="<td>"+false_arr[i].title+"</td>";
+            html+="<td style=background-color:#FF6347>"+false_arr[i].completed+"</td>";
+            html+="</tr>";
+        }
+        html+="</table>"
+        class_name.innerHTML=html;        
+    });
 }
+
+getapi(api_url);
